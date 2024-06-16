c9dc03bc093e714bf219f968577d295c97ca4e1a
a-dishenko <the.teknomad@gmail.com>
2024-06-16 21:34:03 +0300

    cosmetic changes

--- mck_samolet/settings.py
+++ mck_samolet/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1(d(0fmw%56$jz8o*98+ika9!k#sgp9d5m7p6*_k48l5qeevef
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- mck_samolet/wsgi.py
+++ mck_samolet/wsgi.py
@@ -7,9 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
+# -*- coding: utf-8 -*-
+
 import os
+import sys
+import platform
+
+#activate_this = os.path.expanduser('~/venv/bin/activate_this.py')
+#exec(open(activate_this).read(), {'__file__': activate_this})
+
 
 from django.core.wsgi import get_wsgi_application
+
+# путь к проекту
+sys.path.insert(0, '/home/c/ch82151/django_kxnx4/public_html')
+# путь к фреймворку
+sys.path.insert(0, '/home/c/ch82151/django_kxnx4/public_html/mck_samolet')
+# путь к виртуальному окружению
+python_version = ".".join(platform.python_version_tuple()[:2])
+sys.path.insert(0, '/home/c/ch82151/django_kxnx4//django/lib/python{0}/site-packages'.format(python_version))
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mck_samolet.settings')
 
